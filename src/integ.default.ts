1312cd359ea76551a76e1ee4564aa54a3590bed3
WolfAn <andre.wolf@tt-s.com>
2021-05-21 15:24:51 +0200

    - initial commit

---
+++ src/integ.default.ts
@@ -0,0 +1,10 @@
+import { App, Stack } from '@aws-cdk/core';
+import { BuildSpecPipeline } from './BuildSpecPipeline';
+
+const app = new App();
+const stack = new Stack(app, 'tts-cdk-pipelines-integration-test');
+
+//new BuildSpecPipeline(stack, 'BuildPipelineWithDefaults');
+new BuildSpecPipeline(stack, 'BuildPipelineWithProjectName', {
+  projectName: 'tts-cdk-pipelines',
+});
