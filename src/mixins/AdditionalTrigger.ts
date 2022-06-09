8cb5c244a4d8a35b0cbe3f2e8186c77cc292e3e3
WolfAn <andre.wolf@tt-s.com>
2022-06-09 16:08:53 +0200

    feat: added new additional trigger to schedule pipeline execution

--- src/mixins/AdditionalTrigger.ts
+++ src/mixins/AdditionalTrigger.ts
@@ -1,5 +1,5 @@
 import { Stack } from 'aws-cdk-lib';
-import { Rule } from 'aws-cdk-lib/aws-events';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { CodePipeline as CodePipelineTarget } from 'aws-cdk-lib/aws-events-targets';
 import { CodePipeline } from 'aws-cdk-lib/pipelines';
 import { CodePipelineMixin } from './Mixin';
@@ -8,6 +8,27 @@ export abstract class AdditionalTrigger {
 
   public static ssmParameterChange(...parameterNames: string[]): CodePipelineMixin {
     return new SsmParameterChangeTrigger(parameterNames);
+  }
+
+  public static schedule(schedule: Schedule): CodePipelineMixin {
+    return new ScheduledTrigger(schedule);
+  }
+}
+
+class ScheduledTrigger extends CodePipelineMixin {
+
+  readonly schedule: Schedule;
+
+  constructor(schedule: Schedule) {
+    super();
+    this.schedule = schedule;
+  }
+
+  postDoBuildPipeline(_codePipeline: CodePipeline) {
+    new Rule(Stack.of(_codePipeline), 'ScheduledTriggerRule', {
+      schedule: this.schedule,
+      targets: [new CodePipelineTarget(_codePipeline.pipeline)],
+    });
   }
 }
 
